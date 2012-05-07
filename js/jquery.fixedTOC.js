10322ed287d0d2a7d6a277522fb1992ff1a1a0f0
Louis Lazaris <lazaris@impressivewebs.com>
2012-05-07 12:25:46 -0400

    tabs to spaces

--- js/jquery.fixedTOC.js
+++ js/jquery.fixedTOC.js
@@ -2,177 +2,177 @@
     $.fixedTOC = function (el, settings) {
 
         var base = this,
-			s = null;
+            s = null;
 
         base.$el = $(el);
         base.el = el;
 
         // Add a reverse reference to the DOM object
         base.$el.data("fixedTOC", base);
-		base.settings = $.extend({}, $.fixedTOC.defaultSettings, settings);
-		s = base.settings;
+        base.settings = $.extend({}, $.fixedTOC.defaultSettings, settings);
+        s = base.settings;
 
-		if (!s.useSubMenus) {
-			s.tocLinks = '.toc-h1 > a';
-		}
+        if (!s.useSubMenus) {
+            s.tocLinks = '.toc-h1 > a';
+        }
 
-		base.methods = {
+        base.methods = {
 
-			init : function () {
+            init : function () {
 
-				// Put your initialization code here
+                // Put your initialization code here
 
-				s.tocSub.slideUp();
-				s.tocHeight = base.$el.height() + 20;
+                s.tocSub.slideUp();
+                s.tocHeight = base.$el.height() + 20;
 
-				base.$el.css({
-					top: '-' + s.tocHeight + 'px'
-				});
+                base.$el.css({
+                    top: '-' + s.tocHeight + 'px'
+                });
 
-				base.$el.addClass(s.tocUpClass);
+                base.$el.addClass(s.tocUpClass);
 
-			},
+            },
 
-			doOpenMenu : function () {
+            doOpenMenu : function () {
 
-				s.tocLink.on(s.menuOpens, function () {
+                s.tocLink.on(s.menuOpens, function () {
 
-					if (base.$el.hasClass(s.tocUpClass)) {
+                    if (base.$el.hasClass(s.tocUpClass)) {
 
-						s.tocLink.find('span').addClass('rotate');
+                        s.tocLink.find('span').addClass('rotate');
 
-						base.$el.stop().animate({
-							top: '0'
-						}, s.menuSpeed, function () {
-							base.$el.removeClass(s.tocUpClass);
-						});
+                        base.$el.stop().animate({
+                            top: '0'
+                        }, s.menuSpeed, function () {
+                            base.$el.removeClass(s.tocUpClass);
+                        });
 
-					} else {
+                    } else {
 
-						s.tocHeight = base.$el.height() + 20;
+                        s.tocHeight = base.$el.height() + 20;
 
-						s.tocLink.find('span').removeClass('rotate');
+                        s.tocLink.find('span').removeClass('rotate');
 
-						base.$el.stop().animate({
-							top: '-' + s.tocHeight + 'px'
-						}, s.menuSpeed, function () {
-							base.$el.addClass(s.tocUpClass);
-							$('.toc-sub').slideUp(0);
-						});
+                        base.$el.stop().animate({
+                            top: '-' + s.tocHeight + 'px'
+                        }, s.menuSpeed, function () {
+                            base.$el.addClass(s.tocUpClass);
+                            $('.toc-sub').slideUp(0);
+                        });
 
-					}
+                    }
 
-					return false;
-				});
+                    return false;
+                });
 
-			},
+            },
 
-			doOpenItem : function () {
+            doOpenItem : function () {
 
-				$('.toc-h1>a').on('click', function () {
+                $('.toc-h1>a').on('click', function () {
 
-					s.tocSub.each(function () {
+                    s.tocSub.each(function () {
 
-						if (!$(this).hasClass('closed')) {
-							$(this).stop().slideUp().addClass('closed');
-						}
+                        if (!$(this).hasClass('closed')) {
+                            $(this).stop().slideUp().addClass('closed');
+                        }
 
-					});
+                    });
 
-					$(this).parent().find('.toc-sub').stop().slideToggle().removeClass('closed');
+                    $(this).parent().find('.toc-sub').stop().slideToggle().removeClass('closed');
 
-					return false;
+                    return false;
 
-				});
+                });
 
-			},
+            },
 
-			doScroll : function () {
+            doScroll : function () {
 
-				$(s.tocLinks).on('click', function () {
+                $(s.tocLinks).on('click', function () {
 
-					s.currHash = $(this)[0].hash;
+                    s.currHash = $(this)[0].hash;
 
-					$('html, body').animate({
-						scrollTop: $(s.currHash).offset().top - 80
-					}, s.scrollSpeed, function () {
-						location.hash = s.currHash;
-					});
+                    $('html, body').animate({
+                        scrollTop: $(s.currHash).offset().top - 80
+                    }, s.scrollSpeed, function () {
+                        location.hash = s.currHash;
+                    });
 
-					return false;
+                    return false;
 
-				});
+                });
 
-			},
+            },
 
-			doCloseMenu : function () {
+            doCloseMenu : function () {
 
-				$('#toc-holder').on('mouseleave', function () {
+                $('#toc-holder').on('mouseleave', function () {
 
-					s.tocHeight = base.$el.height() + 20;
-					s.tocLink.find('span').removeClass('rotate');
+                    s.tocHeight = base.$el.height() + 20;
+                    s.tocLink.find('span').removeClass('rotate');
 
-					base.$el.animate({
-						top: '-' + s.tocHeight + 'px'
-					}, s.menuSpeed, function () {
-						base.$el.addClass(s.tocUpClass);
-						if (s.resetSubMenus) {
-							$('.toc-sub').slideUp(0);
-						}
-					});
+                    base.$el.animate({
+                        top: '-' + s.tocHeight + 'px'
+                    }, s.menuSpeed, function () {
+                        base.$el.addClass(s.tocUpClass);
+                        if (s.resetSubMenus) {
+                            $('.toc-sub').slideUp(0);
+                        }
+                    });
 
-				});
+                });
 
-			},
+            },
 
-			doTopLink : function () {
+            doTopLink : function () {
 
-				$(s.topLink).on('click', function () {
+                $(s.topLink).on('click', function () {
 
-					s.currHash = $(this)[0].hash;
+                    s.currHash = $(this)[0].hash;
 
-					$('html, body').animate({
-						scrollTop: 0
-					}, s.scrollSpeed, function () {
-						location.hash = s.currHash;
-					});
+                    $('html, body').animate({
+                        scrollTop: 0
+                    }, s.scrollSpeed, function () {
+                        location.hash = s.currHash;
+                    });
 
-					return false;
+                    return false;
 
-				});
+                });
 
-			}
+            }
 
-		};
+        };
 
         // Run methods
         base.methods.init();
-		base.methods.doOpenMenu();
-		if (s.useSubMenus) {
-			base.methods.doOpenItem();
-		}
-		base.methods.doScroll();
-		base.methods.doCloseMenu();
-		if (s.topLinkWorks) {
-			base.methods.doTopLink();
-		}
+        base.methods.doOpenMenu();
+        if (s.useSubMenus) {
+            base.methods.doOpenItem();
+        }
+        base.methods.doScroll();
+        base.methods.doCloseMenu();
+        if (s.topLinkWorks) {
+            base.methods.doTopLink();
+        }
     };
 
     $.fixedTOC.defaultSettings = {
-		// non-customizable settings
-		tocHeight		: null,
-		tocSub			: $('.toc-sub'),
-		tocUpClass		: 'toc-up',
-		tocLink			: $('#toc-link'),
-		tocLinks		: '.toc-h1 ul a',
-		topLink			: $('#top-link'),
-		currHash		: null,
-		// customizable settings
-		scrollSpeed		: 1000,
-		menuSpeed		: 300,
-		useSubMenus		: true,
-		resetSubMenus	: true,
-		topLinkWorks	: true
+        // non-customizable settings
+        tocHeight        : null,
+        tocSub            : $('.toc-sub'),
+        tocUpClass        : 'toc-up',
+        tocLink            : $('#toc-link'),
+        tocLinks        : '.toc-h1 ul a',
+        topLink            : $('#top-link'),
+        currHash        : null,
+        // customizable settings
+        scrollSpeed        : 1000,
+        menuSpeed        : 300,
+        useSubMenus        : true,
+        resetSubMenus    : true,
+        topLinkWorks    : true
     };
 
     $.fn.fixedTOC = function (settings) {
