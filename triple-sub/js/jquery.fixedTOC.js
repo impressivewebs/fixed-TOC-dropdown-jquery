f9eafb82e5463c7e81fe0e440b517089fcb5ae23
Louis Lazaris <lazaris@impressivewebs.com>
2013-09-26 23:33:26 -0400

    more tab fixes

--- triple-sub/js/jquery.fixedTOC.js
+++ triple-sub/js/jquery.fixedTOC.js
@@ -185,7 +185,7 @@
         topLink          : $('#top-link'),
         currHash         : null,
         // customizable settings
-        menuOpens		 : 'click',
+        menuOpens        : 'click',
         scrollSpeed      : 1000,
         menuSpeed        : 300,
         useSubMenus      : true,
